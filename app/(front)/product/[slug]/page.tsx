741f97e32c91c4ccb6ebe5557cf8f44abd152c11
michael <abdulazizklichev1@gamil.com>
2024-04-09 12:10:22 +0900

    create productService and change static.ts to dynamic.ts

--- app/(front)/product/[slug]/page.tsx
+++ app/(front)/product/[slug]/page.tsx
@@ -1,15 +1,36 @@
 import AddToCart from '@/components/products/AddToCart';
 import data from '@/lib/data';
+import productService from '@/lib/services/productService';
+import { convertDocToOb } from '@/lib/utils';
 import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react'
 
-export default function ProductDetails({
+
+export async function generateMetadata({
+    params,
+}:{
+    params: {
+        slug: string;
+    };
+
+}){
+    const product = await productService.getBySlug(params.slug)
+    if(!product) {
+        return {title:'Product not found'}
+    }
+    return{
+        title:product.name,
+        description:product.description,
+    }
+}
+
+export default async function ProductDetails({
     params,
       } :{
         params: { slug: string}
     }) {
-        const product = data.products.find((x) => x.slug === params.slug);
+        const product = await productService.getBySlug(params.slug);
         if (!product) {
             return <div>Product not found</div>;
         }
@@ -70,7 +91,7 @@ export default function ProductDetails({
        {product.countInStock !== 0 && (
         <div className='card-actions justify-center'>
             <AddToCart
-            item={{...product, qty:0, color: '', size:''}}
+            item={{...convertDocToOb(product), qty:0, color: '', size:''}}
             />
         </div>
        )}
--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
           <Header />
         {children}
         <footer className="footer footer-center p-4 bg-base-300 text-base-content" >
-          <p>Copyright 2023 - All right reversed by Next Amazon V2</p>
+          <p>Copyright ©️ 2023 - All right reversed by Next Amazon V2</p>
           </footer>
         </div>
         </body>
